ee679bc2d065fbe2d5d21098a8348aa195740a51
RaulitoUji <al438179@uji.es>
2026-05-26 21:14:15 +0200

    phone

--- components/WhatsAppButton.jsx
+++ components/WhatsAppButton.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 
 const WhatsAppButton = () => {
-  const phoneNumber = '34624636628';
+  const phoneNumber = '34643466617';
   const message = 'Hola. Me interesa conocer más sobre sus servicios, ¿podrían brindarme más información? Muchas gracias.';
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
